96e7d724d75deac78f5f2bbb5b01a665d1741381
Utkarshverma7 <utkarshverma1947@gmail.com>
2022-04-19 19:19:42 +0530

    api2

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const api="694fc8b3d5f5161b0177731528958f70";
+const api="api=694fc8b3d5f5161b0177731528958f70";
+const base="https://api.themoviedb.org/3";
+const api_url=base + "/discover/movie?sort_by=popularity.desc&"+api;
+
+function getmovies(api_url){
+    fetch(url).then(res=>json()).then(data=>{
+        console.log(data);
+    })
+}
